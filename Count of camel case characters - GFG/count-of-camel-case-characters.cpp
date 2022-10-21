cf1387e17bf13ea6bf8aa797f59caaae026648e8
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-21 18:22:38 +0530

    Added solution - LeetHub

---
+++ Count of camel case characters - GFG/count-of-camel-case-characters.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+  public:   
+    int countCamelCase (string s)
+    {
+    	//code here.
+    	int count = 0;
+    	for(int i = 0; i < s.size(); i++) {
+    	    if(s[i] >= 65 && s[i] <= 90)
+    	    count++;
+    	}
+    	return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout <<ob.countCamelCase (s) << '\n';
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
