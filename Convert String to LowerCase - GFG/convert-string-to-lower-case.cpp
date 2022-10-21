5205e06da6fd896204db36f4f85beffaea1aa3a3
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-10-21 18:28:31 +0530

    Added solution - LeetHub

---
+++ Convert String to LowerCase - GFG/convert-string-to-lower-case.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string S) {
+        // code here
+        for(int i = 0; i < S.size(); i++){
+            if(S[i] < 97) {
+                S[i] = S[i] + 32;
+            }
+        }
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.toLower(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
