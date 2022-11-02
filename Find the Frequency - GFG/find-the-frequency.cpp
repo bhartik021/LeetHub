62dc86da8bccc32b2887639f28c2af8a2a7fb0cd
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2022-11-02 23:02:30 +0530

    Added solution - LeetHub

--- Find the Frequency - GFG/find-the-frequency.cpp
+++ Find the Frequency - GFG/find-the-frequency.cpp
@@ -12,12 +12,25 @@ using namespace std;
 */
 int findFrequency(vector<int> v, int x){
     // Your code here
-    int count = 0;
-    for(int i = 0 ; i < v.size(); i++) {
+    
+    //Brute force approcah
+    // int count = 0;
+    // for(int i = 0 ; i < v.size(); i++) {
+    //     if(v[i] == x)
+    //     count++;
+    // }
+    // return count;
+    
+    //using map
+    unordered_map<int, int>mp;
+    for(int i = 0; i < v.size(); i++) {
         if(v[i] == x)
-        count++;
+        mp[x]++;
     }
-    return count;
+    if(mp.size() != 0) {
+        return mp[x];
+    }
+    return 0;
 }
 
 //{ Driver Code Starts.
