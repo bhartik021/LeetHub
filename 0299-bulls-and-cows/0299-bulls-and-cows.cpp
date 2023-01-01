49de017483a611fc389c71c6ac2ec6e89eca8b43
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-01-01 23:19:05 +0530

    Time: 3 ms (91.80%), Space: 6.6 MB (42.33%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0;
+        int cows = 0;
+        vector<int> s(10, 0);
+        vector<int> g(10, 0);
+        
+        for(int i = 0; i < secret.length(); i++) {
+            if(secret[i] == guess[i]) {
+                bulls++;
+            }else {
+                s[secret[i] - '0']++;
+                g[guess[i] - '0']++;
+            }
+        }
+        for(int i = 0; i < 10; i++) {
+            cows+=min(s[i], g[i]);
+        }
+        string ans = "";
+        ans+=to_string(bulls);
+        ans+='A';
+        ans+=to_string(cows);
+        ans+='B';
+        
+        return ans;
+    }
+};
