228cde1b75c4c54087a26841c9cfbb47303802a8
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-01-18 19:53:37 +0530

    Time: 182 ms (41.81%), Space: 84.4 MB (91.73%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class BSTIterator {
+ public:
+  BSTIterator(TreeNode* root) {
+    pushLeftsUntilNull(root);
+  }
+
+  int peek() {
+    return stack.top()->val;
+  }
+
+  void next() {
+    TreeNode* node = stack.top(); stack.pop();
+    pushLeftsUntilNull(node->right);
+  }
+
+  bool hasNext() {
+    return !stack.empty();
+  }
+
+ private:
+  stack<TreeNode*> stack;
+
+  void pushLeftsUntilNull(TreeNode* node) {
+    while (node) {
+      stack.push(node);
+      node = node->left;
+    }
+  }
+};
+
+class Solution {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+    vector<int> ans;
+    BSTIterator bstIterator1(root1);
+    BSTIterator bstIterator2(root2);
+
+    while (bstIterator1.hasNext() && bstIterator2.hasNext())
+      if (bstIterator1.peek() < bstIterator2.peek()) {
+        ans.push_back(bstIterator1.peek());
+        bstIterator1.next();
+      } else {
+        ans.push_back(bstIterator2.peek());
+        bstIterator2.next();
+      }
+
+    while (bstIterator1.hasNext()) {
+      ans.push_back(bstIterator1.peek());
+      bstIterator1.next();
+    }
+
+    while (bstIterator2.hasNext()) {
+      ans.push_back(bstIterator2.peek());
+      bstIterator2.next();
+    }
+
+    return ans;  
+    }
+};
