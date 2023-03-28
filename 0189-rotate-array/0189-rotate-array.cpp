e3bfb32ee6abf77a791593bcf16a394a65896941
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-03-28 20:41:12 +0530

    Time: 37 ms (30.35%), Space: 25 MB (85.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-    vector<int>temp(nums.size());
+//     vector<int>temp(nums.size());
         
-        for(int i = 0; i < nums.size(); i++) {
-            temp[(i+k) % nums.size()] = nums[i];
-        }
+//         for(int i = 0; i < nums.size(); i++) {
+//             temp[(i+k) % nums.size()] = nums[i];
+//         }
         
-        nums = temp;
+//         nums = temp;
     
      // Time Complexity => O(N)
      // Space Complexity => O(1) 
-     // k %= nums.size();
-     //    reverse(nums.begin(), nums.end());
-     //    reverse(nums.begin(), nums.begin() + k);
-     //    reverse(nums.begin() + k, nums.end());
+     k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
