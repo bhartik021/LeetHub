cd49dd6e5c1c527a97cb121154000ceaef05f1f5
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-04-12 22:15:32 +0530

    Time: 8 ms (46.38%), Space: 9.4 MB (70.22%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+     string ret, tmp;
+        vector<string> stack;
+        for ( istringstream iss(path); getline(iss, tmp, '/');  ) {
+            if (tmp == ".." && !stack.empty()) stack.pop_back();
+            else if (tmp == "." || tmp == ".." || tmp == "") continue;
+            else stack.push_back(tmp);
+        }
+        for (auto str : stack) { ret += "/" + str; }
+        return ret.empty() ? "/" : ret;   
+    }
+};
