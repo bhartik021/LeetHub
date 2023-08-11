0e136cfbda8dcc87b430d50b53c2c2d889fcf203
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-08-12 00:41:40 +0530

    Time: 43 ms (97.44%), Space: 42 MB (45.81%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const ans = [];
+        for(let i = 0; i < arr.length; i++) 
+        ans[i] = fn(arr[i], i);
+    return ans;
+};
