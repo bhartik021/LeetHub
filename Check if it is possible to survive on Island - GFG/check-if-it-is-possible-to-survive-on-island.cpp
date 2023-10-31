af2258776836792f35af2832ccddbd0220950d71
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-10-31 11:11:59 +0530

    Added solution - LeetHub

--- Check if it is possible to survive on Island - GFG/check-if-it-is-possible-to-survive-on-island.cpp
+++ Check if it is possible to survive on Island - GFG/check-if-it-is-possible-to-survive-on-island.cpp
@@ -7,28 +7,53 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    int minimumDays(int S, int N, int M){
-        // code here
-        int sunday = S/7;
+// class Solution{
+// public:
+//     int minimumDays(int S, int N, int M){
+//         // code here
+//         int sunday = S/7;
+//         int buyingDays = S - sunday;
+//         int totalFood = S * M;
+//         int ans = 0;
+        
+//         if(totalFood % N == 0)  {
+//             ans = totalFood / N;
+//         }else {
+//             ans = totalFood / N + 1;
+//         }
+        
+//         if(ans <= buyingDays)  {
+//             return ans;
+//         }else {
+//             return -1;
+//         }
+//     }
+// };
+
+
+class Solution {
+    public:
+    int minimumDays(int S, int N, int M) {
+        int sunday = S / 7;
         int buyingDays = S - sunday;
         int totalFood = S * M;
+        
         int ans = 0;
         
-        if(totalFood % N == 0)  {
+        if(totalFood % N == 0) {
             ans = totalFood / N;
         }else {
             ans = totalFood / N + 1;
         }
         
-        if(ans <= buyingDays)  {
+        if(ans <= buyingDays) {
             return ans;
         }else {
             return -1;
         }
     }
 };
+
 
 //{ Driver Code Starts.
 
