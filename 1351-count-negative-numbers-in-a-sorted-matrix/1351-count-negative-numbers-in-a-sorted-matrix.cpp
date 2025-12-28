b4467faaaf46b81ddd4acae33c57f81f303fcb71
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2025-12-28 21:24:44 +0530

    Time: 0 ms (100%), Space: 14 MB (91.47%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -4,10 +4,9 @@ public:
     int count = 0;
     for(int i = 0; i < grid.size(); i++) {
         for(int j = 0; j < grid[0].size(); j++) {
-            if(grid[i][j] < 0) 
-            count++;
+            if(grid[i][j] < 0) count++;
         }
-    }     
-    return count;
+    }  
+    return count;  
     }
 };
