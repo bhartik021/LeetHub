2cd45aaf3ed7f56af9f86e45096966a7d097dce0
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-01-10 18:14:11 +0530

    Time: 0 ms (100%), Space: 59.4 MB (45.93%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -15,6 +15,6 @@ public:
     if(!root) return new TreeNode(val);
     if(val < root->val) root->left = insertIntoBST(root->left, val);
     else root->right = insertIntoBST(root->right, val);
-    return root;
+    return root;    
     }
 };
