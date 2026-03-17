7bde762a2279087a7a4d2d8b1c2f93ccce38577a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-03-17 22:38:48 +0530

    Time: 12 ms (6.58%), Space: 12 MB (29.23%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-    map<char, int>mp; 
-    for(char c : magazine) {
-        mp[c]++;
+    map<char, int>mp;
+
+    for(auto & s : magazine) {
+        mp[s]++;
+    }    
+
+    for(auto & t : ransomNote) {
+        mp[t]--;
+        if(mp[t] < 0) return false;
     }
 
-    for(char c : ransomNote) {
-        mp[c]--;
-        if(mp[c] < 0) return false;
-    }
     return true;
     }
 };
