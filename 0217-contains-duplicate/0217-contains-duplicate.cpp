a07be2ad359f8be1b758892887f6a1a52477419e
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-05-04 14:55:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
     for(int i = 0; i < nums.size() - 1; i++) {
-        if(nums[i] == nums[i+1]) 
-        return true;
-    }    
-    return false;
+        for(int j = i + 1; j < nums.size(); j++) {
+            if(nums[i] == nums[j]) return true;
+        }
+    }  
+    return false;  
     }
+    // sort(nums.begin(), nums.end());
+    // for(int i = 0; i < nums.size() - 1; i++) {
+    //     if(nums[i] == nums[i+1]) 
+    //     return true;
+    // }    
+    // return false;
+    // }
 };
