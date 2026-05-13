894ffe2da56c71af7f2f73844d68fbc6f95eb550
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-05-13 23:21:16 +0530

    Time: 110 ms (33.29%), Space: 76.6 MB (25.76%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-    sort(nums.begin(), nums.end());
-    int start = 0, end = nums.size() - 1;
+    // with soritng O(n log n)
+    // sort(nums.begin(), nums.end());
+    // int start = 0, end = nums.size() - 1;
+    // int count = 0;
+    // while(start < end) {
+    //     int sum = nums[start] + nums[end];
+    //     if(sum == k) {
+    //         count++;
+    //         start++;
+    //         end--;
+    //     }else if(sum > k) {
+    //         end--;
+    //     }else {
+    //         start++;
+    //     }
+    // }    
+    // return count;
+
+    unordered_map<int, int>mp;
     int count = 0;
-    while(start < end) {
-        int sum = nums[start] + nums[end];
-        if(sum == k) {
+    for(int num : nums) {
+        int need = k - num;
+        if(mp[need] > 0) {
             count++;
-            start++;
-            end--;
-        }else if(sum > k) {
-            end--;
+            mp[need]--;
         }else {
-            start++;
+            mp[num]++;
         }
-    }    
+    }
     return count;
     }
 };
