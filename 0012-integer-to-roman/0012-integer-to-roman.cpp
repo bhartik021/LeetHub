0d90753e1a6287d467cf095eeb4233ee556cd8be
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-05-18 23:39:12 +0530

    Time: 3 ms (66.36%), Space: 11.9 MB (67.21%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
     string intToRoman(int num) {
-    map<int, string, greater<int>> mp = {
-        {1000, "M"},
-        {900, "CM"},
-        {500, "D"},
-        {400, "CD"},
-        {100, "C"},
-        {90, "XC"}, 
-        {50, "L"},
-        {40, "XL"}, 
-        {10, "X"}, 
-        {9, "IX"},
-        {5, "V"}, 
-        {4, "IV"},
-        {1, "I"}
-    };
+    // using hashmap
+    // map<int, string, greater<int>> mp = {
+    //     {1000, "M"},
+    //     {900, "CM"},
+    //     {500, "D"},
+    //     {400, "CD"},
+    //     {100, "C"},
+    //     {90, "XC"}, 
+    //     {50, "L"},
+    //     {40, "XL"}, 
+    //     {10, "X"}, 
+    //     {9, "IX"},
+    //     {5, "V"}, 
+    //     {4, "IV"},
+    //     {1, "I"}
+    // };
+
+    // string result = "";
+    // for(auto it : mp) {
+    //     int value = it.first;
+    //     string symbol = it.second;
+
+    //     while(num >= value) {
+    //         result += symbol;
+    //         num -= value;
+    //     }
+    // }
+    // return result;
+
+    vector<int>value={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string>symbol={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     string result = "";
-    for(auto it : mp) {
-        int value = it.first;
-        string symbol = it.second;
 
-        while(num >= value) {
-            result += symbol;
-            num -= value;
+    for(int i = 0; i < value.size(); i++) {
+        while(num >= value[i]) {
+            result += symbol[i];
+            num -= value[i];
         }
     }
     return result;
