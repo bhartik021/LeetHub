b8ba9fecea6377308a0674fdc6c0a69fd7cdb93a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-05-18 23:32:02 +0530

    Time: 10 ms (12.96%), Space: 16.1 MB (5.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
-    // unordered_map<char, int> mp = {
-    //     {'I', 1},
-    //     {'V', 5},
-    //     {'X', 10},
-    //     {'L', 50},
-    //     {'C', 100},
-    //     {'D', 500}, 
-    //     {'M', 1000}
-    // }    
-
     map<int, string, greater<int>> mp = {
         {1000, "M"},
         {900, "CM"},
         {500, "D"},
         {400, "CD"},
         {100, "C"},
-        {90, "XC"},
+        {90, "XC"}, 
         {50, "L"},
-        {40, "XL"},
-        {10, "X"},
+        {40, "XL"}, 
+        {10, "X"}, 
         {9, "IX"},
         {5, "V"}, 
         {4, "IV"},
@@ -28,7 +18,6 @@ public:
     };
 
     string result = "";
-
     for(auto it : mp) {
         int value = it.first;
         string symbol = it.second;
