12cfb9261d12726d4e8afdceb4c4774f2ff3ee9f
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-05-27 01:05:05 +0530

    Time: 3 ms (30%), Space: 10.3 MB (23.47%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    string clean = "";
+    for(char c : s) {
+        if(isalnum(c)) {
+            clean += tolower(c);
+        }
+    }    
+    int n = clean.size();
+    for(int i = 0; i < n/2; i++) {
+        if(clean[i] != clean[n-1-i]) {
+            return false;
+        }
+    }
+    return true;
+    }
+};
