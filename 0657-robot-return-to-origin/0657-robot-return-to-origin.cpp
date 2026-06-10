ca051e1adce529676866f6aaf596e9e9a26daeb4
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-06-10 23:20:41 +0530

    Time: 4 ms (26.77%), Space: 10.4 MB (44.49%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
+    // tc -> O(n) sc -> O(1)
+    // do left and right cancel each other
+    // do up and down cancel each other
     int x = 0, y = 0;
     for(char c : moves) {
         if(c == 'R') x++;
