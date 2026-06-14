9a6c745df7a8072e8e8fec95aa65a82fc72408dd
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-06-14 19:17:36 +0530

    Time: 2 ms (36.35%), Space: 10.4 MB (12.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -6,14 +6,11 @@ public:
         if(isalnum(c)) {
             clean += tolower(c);
         }
-    }    
+    }   
 
     int n = clean.size();
-
     for(int i = 0; i < n/2; i++) {
-        if(clean[i] != clean[n-1-i]) {
-            return false;
-        }
+        if(clean[i] != clean[n - 1 - i]) return false;
     }
     return true;
     }
