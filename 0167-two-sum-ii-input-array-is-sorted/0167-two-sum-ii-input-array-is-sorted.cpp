c58044de3d4130e71aab34ce572c92fe16d5615c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-06-15 18:53:09 +0530

    Time: 0 ms (100%), Space: 19.6 MB (33.16%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-    // O(n)^2
+    // O(n^2)
     // for(int i = 0; i < numbers.size() - 1; i++) {
     //     for(int j = i + 1; j < numbers.size(); j++) {
-    //         if(numbers[i] + numbers[j] == target) return {i + 1, j + 1};
+    //         if(numbers[i] + numbers[j] == target) return {i + 1, j + 1}; 
     //     }
-    // }    
+    // }  
     // return {};
 
+    // two pointers
     int start = 0, end = numbers.size() - 1;
     while(start < end) {
         int sum = numbers[start] + numbers[end];
