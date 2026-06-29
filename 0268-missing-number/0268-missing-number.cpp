854bf8f342cee48bf225d2616247499fe2d4cec6
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-06-29 23:13:31 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.9%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
     // tc - o(n), sc - o(1)
-    int n = nums.size();
-    int expectedSum = (n * (n + 1)) / 2;
-    int actualSum = 0;
-    for(int num : nums) {
-        actualSum += num;
-    }    
-    return expectedSum - actualSum;
+    // int n = nums.size();
+    // int expectedSum = (n * (n + 1)) / 2;
+    // int actualSum = 0;
+    // for(int num : nums) {
+    //     actualSum += num;
+    // }    
+    // return expectedSum - actualSum;
+
+    // with bit manipulation
+    int ans = nums.size();
+    for(int i = 0; i < nums.size(); i++) {
+        ans ^= i ^ nums[i];
+    }
+    return ans;
     }
 };
