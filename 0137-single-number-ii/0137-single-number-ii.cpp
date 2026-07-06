f3a9b274cb9f09caad7e06eb2ab58720bea45f18
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-06 22:42:14 +0530

    Time: 4 ms (13.93%), Space: 14.2 MB (10.69%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    unordered_map<int, int>mp;
+    for(int num : nums) {
+        mp[num]++;
+    }    
+
+    for(auto m : mp) {
+        if(m.second == 1) return m.first;
+    }
+    return -1;
+    }
+};
