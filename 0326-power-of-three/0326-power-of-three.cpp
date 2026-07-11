e884a2e959d0f1d438988190e5d66143c1aba617
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-11 23:24:00 +0530

    Time: 3 ms (59.07%), Space: 8.9 MB (74.7%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
     if(n <= 0) return false;
-    return (n % 3 == 1) == 0;    
+    while(n % 3 == 0) {
+        n/=3;
+    }   
+    return n == 1;
     }
 };
