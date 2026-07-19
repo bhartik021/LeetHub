1f0637bf79fdb0a4eb16a8d70c8eba13a0d334f6
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-20 00:51:19 +0530

    Time: 4 ms (42.06%), Space: 12.6 MB (37.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,27 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        # create a empty python list as a stack
         stack = []
 
+        # create dictionary of matcing the pattern rather than if-else
         pairs = {
             ')' : '(',
             '}' : '{',
             ']' : '['
         }
 
+        # iterate over the charcters
         for ch in s:
-            # opeming bracket
+            # if it is opening bracket then push in stack
             if ch in "({[":
                 stack.append(ch)
 
+            # if it is closing brakcet
             else:
                 if not stack:
                     return False
 
                 if stack[-1] != pairs[ch]:
                     return False
+                    
                 stack.pop()
 
         return len(stack) == 0
-
         
