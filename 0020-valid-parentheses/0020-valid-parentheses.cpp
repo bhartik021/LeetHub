8bac9f07973432388e3eda0909db4a0bf3ef895d
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-19 22:01:11 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+    stack<char>st;
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]);
+        }   
+
+        if(s[i] == ')') {
+            if(!st.empty() && st.top() == '(') {
+                st.pop();
+            }else {
+                return false;
+            }
+        }
+
+        if(s[i] == '}') {
+            if(!st.empty() && st.top() == '{') {
+                st.pop();
+            }else {
+                return false;
+            }
+        }
+
+        if(s[i] == ']') {
+            if(!st.empty() && st.top() == '[') {
+                st.pop();
+            }else {
+                return false;
+            }
+        }
+    }
+
+    if(st.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+    }
+};
