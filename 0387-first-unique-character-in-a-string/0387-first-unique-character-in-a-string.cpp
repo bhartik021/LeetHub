6552a5de03e479201b948d543a1025ffb98e48a5
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-24 23:32:53 +0530

    Time: 12 ms (44.49%), Space: 14.8 MB (56.49%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -4,10 +4,12 @@ public:
     unordered_map<char, int>mp;
     for(char c : s) {
         mp[c]++;
-    }  
+    }
 
     for(int i = 0; i < s.size(); i++) {
-        if(mp[s[i]] == 1) return i;
+        if(mp[s[i]] == 1) {
+            return i;
+        }
     }
     return -1;
     }
