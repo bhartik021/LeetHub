8f18400aa89f0e2c44678276ca6956fc9da4be9f
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-25 19:18:33 +0530

    Time: 0 ms (100%), Space: 10 MB (11.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,13 +5,12 @@ public:
     unordered_map<char, int>mp;
     for(char ss : s) {
         mp[ss]++;
-    }    
+    }      
 
     for(char tt : t) {
         mp[tt]--;
         if(mp[tt] < 0) return false;
-    } 
-
+    }
     return true;
     }
 };
