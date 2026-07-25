7213897bc8fd3023e1e41917a0acf14c5984649f
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-25 18:32:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.64%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+    int count = 0;
+    unordered_set<char>jset;
+    for(char j : jewels) {
+        jset.insert(j);
+    }
+    for(char s : stones) {
+        if(jset.count(s)) count++;
+    }
+    return count;
+    }
+};
