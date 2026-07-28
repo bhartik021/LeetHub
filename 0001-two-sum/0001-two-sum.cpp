620383c755218f296ebdb55b26bb25d862e76338
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-28 20:04:01 +0530

    Time: 43 ms (24.14%), Space: 14 MB (83.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+    // time complexity O(n)^2
     for(int i = 0; i <= nums.size() - 2; i++) {
         for(int j = i + 1; j <= nums.size() - 1; j++) {
             if(nums[i] + nums[j] == target) return {i, j};
