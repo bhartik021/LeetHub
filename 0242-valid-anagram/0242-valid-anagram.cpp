8f9cc52f892707512c4e86c126d2a95edb29afd7
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-28 19:32:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
     if(s.size() != t.size()) return false;
+
     unordered_map<char, int>mp;
+
     for(char ss : s) {
         mp[ss]++;
-    }      
+    }    
 
     for(char tt : t) {
         mp[tt]--;
+
         if(mp[tt] < 0) return false;
+
     }
+
     return true;
     }
 };
