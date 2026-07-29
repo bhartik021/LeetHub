4fc147692d224147b57476b64f452e7d98fcd87a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-30 03:58:24 +0530

    Time: 6 ms (19.11%), Space: 20 MB (11.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-    // will store numbers along with its freq in map
+    // will use bucker sort here for  O(n log n ) time complexity
+    // first will create map to store number along with its freq
     unordered_map<int, int>mp;
     for(int num : nums) {
         mp[num]++;
-    }  
+    } 
 
-    // will create bucket here of give array size + 1
-    vector<vector<int>>bucket(nums.size() + 1);
+    // now will create bucket here to store the number on its index (which is equal to freq of its number) this will be of array size + 1
+    vector<vector<int>> bucket(nums.size() + 1);
+    // will iterate over the map to store the element in bucker 
+    for(auto it : mp) {
+        bucket[it.second].push_back(it.first);
+    }
 
-        // iterate over map and push element in the bucket 
-        for(auto it : mp) {
-            bucket[it.second].push_back(it.first);
-        }
-
-        // will return this vector ans
-        vector<int>ans;
-
-        // will taverse over bucket from back side
-        for(int i = bucket.size() - 1; i >= 0 && ans.size() < k; i--) {
-            for(int num: bucket[i]) {
-                ans.push_back(num);
-                if(ans.size() == k) break;
-            }    
-        }
-        return ans;
+    // will create vector to returnt the output
+    vector<int>ans;
+    // will iterate over bucket from back side till 0th and will stop till k size
+    for(int i = bucket.size() - 1; i >= 0 && ans.size() < k; i--) {
+        for(int num : bucket[i]) {
+            ans.push_back(num);
+            // if return vector size is equal to k will stop to push element in the ans vector
+            if(ans.size() == k) break;
+        } 
+    }
+    return ans;
     }
 };
