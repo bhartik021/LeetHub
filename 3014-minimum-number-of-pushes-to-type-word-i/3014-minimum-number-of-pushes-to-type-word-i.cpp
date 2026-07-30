c5a6078042487a961a1dba131de606d9eb178e73
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-07-30 23:06:17 +0530

    Time: 1 ms (19.68%), Space: 8.4 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+    // t.c => 0(N) , S.C.=> O(1)
     int ans = 0;
     for(int i = 0; i < word.size(); i++) {
         ans += i / 8 + 1;
