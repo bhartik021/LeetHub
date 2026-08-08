9f9012046b0e3c372e5fd2f69e160776b99c5a91
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-08 19:01:35 +0530

    Time: 20 ms (46.89%), Space: 91 MB (79.7%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
-    // will store the empty spaces in each bag by finding the difference b/w current capacity - occupied capacity
-    vector<int>vacancy(capacity.size());  
+    // maximum number of full capacity bags
+    int fullCapacityMaximumBags = 0;
+
+    // will store number of space remaining in each bag
+    vector<int>vacancy(capacity.size()); 
+
+    // iterate over the capacity array to find how much space is remaining in current bag
 
     for(int i = 0; i < capacity.size(); i++) {
-        vacancy[i] = capacity[i] - rocks[i];
+        // will find the differenc b/w whole space - current occupied space
+        vacancy[i] = capacity[i] - rocks[i]; 
     }  
 
-    // will sort vacancy array
+    // now we will sort the vacancy array
     sort(vacancy.begin(), vacancy.end());
 
-    // maximum fullCapacityBags that needs to be return
-    int fullCapacityBags = 0;
-
+    // iterate over the vacancy array to find which can be fullfill
     for(int i = 0; i < vacancy.size(); i++) {
+        // case 1 : if vacancy is aleday full then it is already have fullcapacity no additionalrocks can be added here 
         if(vacancy[i] == 0) {
-            fullCapacityBags++;
-        } else if(additionalRocks >= vacancy[i]) {
+            fullCapacityMaximumBags++;
+        // case 2 : if additionalRocks is greater than current vacancy[i] only then it can go inside vacancy[i]
+        }else if(additionalRocks >= vacancy[i]) {
+            // will also remove current space form vacancy whatever additionalRocks we have put in vacancy[i]
             additionalRocks -= vacancy[i];
-            fullCapacityBags++;
-        } else {
+            // increase the fullCapacityMaximumBags count
+            fullCapacityMaximumBags++;
+        // case 3 : id there is no additionalRocks there is nothing to be assigned in the vancancy
+        }else {
             break;
-        }
+        }  
     }
-    return fullCapacityBags;
+    return fullCapacityMaximumBags;
     }
 };
