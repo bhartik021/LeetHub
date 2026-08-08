9f4a9098066aaf59a4e8f2504570bf6db15dde66
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-08 22:01:58 +0530

    Time: 50 ms (49%), Space: 93.8 MB (91.35%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
+    // comparator to sort the intervals based on the end time
     static bool compare(const vector<int>&a, const vector<int>&b) {
         return a[1] < b[1];
     }
 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    // greedy approach
-    // will sort intervals based on the end time of each interval
-    sort(intervals.begin(),intervals.end(), compare);  
+    // greedy approch , t.c => O(n log n + n) , s.c => O(1)
 
+    // sort the intevrals array based on the end time along with comparator
+    sort(intervals.begin(), intervals.end(), compare); 
+
+    // count to store how many meetings can be attended , also first meeting will be always attended so count wll be 1
     int count = 1;
+
+    // it will store last meeting end time of each interval so that we can check there is no overlap of upcoming start time
+    // also at first we will store first interval
     int lastEndingTime = intervals[0][1];
 
-    for(int i = 0; i < intervals.size(); i++) {
+    // iterate over the intervals
+    for(int i = 1; i < intervals.size(); i++) {
+        // if last meeting end time is less than of equal to next interval start time will increaed the counter
         if(intervals[i][0] >= lastEndingTime) {
             count++;
+            // also last meeting time will be updated with next intervals end time
             lastEndingTime = intervals[i][1];
         }
     } 
-
-    return intervals.size() - count; 
+    // at the end will return subtraction of overall meetiings and total meeting attented // so that we can get number of not attenting meetings
+    return intervals.size() - count;  
     }
 };
