be00e2532eaae02e1738cefa9894939bb413075e
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-08 11:38:03 +0530

    Time: 11 ms (39.31%), Space: 44.9 MB (12.45%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,24 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
     sort(g.begin(), g.end());
     sort(s.begin(), s.end());
-    int l = 0, r = 0;
-    while(l < g.size() && r < s.size()) {
-        if(g[l] <= s[r]) {
-            l++;
-        }
-        r++;
-    }    
-    return l;
+    int left = 0, right = 0;
+    while(left < g.size() && right < s.size()) {
+        if(g[left] <= s[right]) {
+            left++;
+        } 
+        right++;
+    } 
+    return left;  
     }
 };
-
-// children cookie
-// 1 2 3 4  1 3 3
-// 1        1 
-// l        r
-// l++      r++
-// 2        3
-// l++      r++
-// 3        3 
-// l++      r++ 
-// 4 stop
