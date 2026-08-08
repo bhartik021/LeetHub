75ecb16eb5a6b52dfd4cef3239588d1b8239487d
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-08 12:26:37 +0530

    Time: 11 ms (39.31%), Space: 44.8 MB (48.87%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+    // greedy appraoch
+    // time complexity => O(n log n + m log m)
+    // space complexity => O(log n log m)
     sort(g.begin(), g.end());
-    sort(s.begin(), s.end());
+    sort(s.begin(), s.end());    
     int left = 0, right = 0;
     while(left < g.size() && right < s.size()) {
         if(g[left] <= s[right]) {
             left++;
-        } 
+        }
         right++;
-    } 
-    return left;  
+    }
+    return left;
     }
 };
