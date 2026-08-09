20002bf0fb9007e8d50ba0629262cfdaabaef838
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-09 19:01:03 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    // custom comparator to sort the 2D vector based on the ending index in acessnding order
+    // custom comparator to sort 2D vector based on the start time of eacg interval in assceding order
     static bool compare(const vector<int>&a, const vector<int>&b) {
         return a[0] < b[0];
     }
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-    // need to be return 
+    // create an vector of merged intervals that needs to be return
     vector<vector<int>>mergedIntervals;
 
-    // sort all intervals based on the ending time along with custom comparator
+    // will sort intervals based on the start time of each intervals along with custom comparator
     sort(intervals.begin(), intervals.end(), compare);
 
-    // Add the first interval
+    // first will push first interval in mergedIntervals vector
     mergedIntervals.push_back(intervals[0]);
 
-    // iterate over the intervals vector
+    // iterate over the all intervals
     for(int i = 1; i < intervals.size(); i++) {
-        // Last interval in our mergedIntervals vector
-        int lastEnd = mergedIntervals.back()[1];
-
-        // if current interval overlaps
-        if(intervals[i][0] <= lastEnd) {
-            // extend the ending time of previous interval
-            mergedIntervals.back()[1] = max(lastEnd, intervals[i][1]);
+        int lastIntervalEndingTime = mergedIntervals.back()[1];
+        //  if current interval start time is lesser than ending time of previos interval
+        if(intervals[i][0] <= lastIntervalEndingTime) {
+            mergedIntervals.back()[1] = max(lastIntervalEndingTime, intervals[i][1]);
         }else {
-            // if there is no overlap will push interval in mergedIntervals vector
-            mergedIntervals.push_back(intervals[i]);
+            // if there is no overlaap simply push that interval in mergedIntervals vector
+            mergedIntervals.push_back(intervals[i]); 
         }
     }
     return mergedIntervals;
