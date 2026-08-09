e0e945b748776384b223c6e185f1133b62fdf95c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-09 18:25:24 +0530

    Time: 48 ms (54.83%), Space: 93.8 MB (81.09%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    // custom comparator to sort based on ending index
-    static bool compare(const vector<int>&a , const vector<int>&b) {
+    // custom comparator to sort 2D vector based on the ending index in asscending order
+    static bool compare(const vector<int>&a, const vector<int>&b) {
         return a[1] < b[1];
     }
 
     int findMinArrowShots(vector<vector<int>>& points) {
+    // greedy approach 
+    // time complexity => O(n log n)
+    // space complexity => O(1)   
+
+    // if there are no poitns simply no number of arrows needed to burst ballons so would return 0
+
     if(points.size() == 0) return 0;
 
-    // sort based on end index along with comparator function
-    sort(points.begin(), points.end(), compare); 
+    // first we will sort the all points based on the ending index along with custom comparator fun
+    sort(points.begin(), points.end(), compare);
 
-    int needArrows = 1;
+    // 1 arrows always be needed to burst the ballon
+    int numberOfArrowsNeeded = 1;
+    // ending point of each point to check if there is no overlap with next point of starting point, it will be initialized with first point endiing point
     int endingPoint = points[0][1];
 
+    // iterate over the points vector from 1 index becuase we have intialized endpoint with first point ending point and arrow is also 1
     for(int i = 1; i < points.size(); i++) {
         if(points[i][0] > endingPoint) {
-            needArrows++;
+            numberOfArrowsNeeded++;
             endingPoint = points[i][1];
         }
-    }  
-    return needArrows; 
+    }
+    return numberOfArrowsNeeded;
     }
 };
