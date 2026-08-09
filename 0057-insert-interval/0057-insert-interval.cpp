264f64872ff08e64dabc1ca1120a0e6f17879c3d
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-09 23:27:27 +0530

    Time: 4 ms (17.64%), Space: 21.6 MB (76.98%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    // greedy approach
     // time complexity => O(n)
     // space complexity => O(n)
-    vector<vector<int>>result;
-    int i = 0, n = intervals.size(); 
-    // case 0: no overlaping
+    vector<vector<int>> result;
+    int i = 0, n = intervals.size();  
+    // case 1 : the left part of the intervals
     while(i < n && intervals[i][1] < newInterval[0]) {
         result.push_back(intervals[i]);
         i++;
-    }    
-    // case 1 : overlapping
+    }  
+
+    // case 2 : the mid part where intervals are overlapping with newintervals
     while(i < n && intervals[i][0] <= newInterval[1]) {
-        newInterval[0] = min(newInterval[0], intervals[i][0]);
-        newInterval[1] = max(newInterval[1], intervals[i][1]);
+        newInterval[0] = min(intervals[i][0], newInterval[0]);
+        newInterval[1] = max(intervals[i][1], newInterval[1]);
         i++;
     }
     result.push_back(newInterval);
 
-    // case 3
+    // case 3 : the right part as it as
     while(i < n) {
         result.push_back(intervals[i]);
         i++;
