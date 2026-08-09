35814c4e8a5d3a12561095edebb9bd202f3408ec
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-09 22:52:15 +0530

    Time: 1 ms (36.51%), Space: 21.7 MB (48.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
-        int n = intervals.size(), i = 0;
-        vector<vector<int>> res;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    // time complexity => O(n)
+    // space complexity => O(n)
+    vector<vector<int>>result;
+    int i = 0, n = intervals.size(); 
+    // case 0: no overlaping
+    while(i < n && intervals[i][1] < newInterval[0]) {
+        result.push_back(intervals[i]);
+        i++;
+    }    
+    // case 1 : overlapping
+    while(i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = min(newInterval[0], intervals[i][0]);
+        newInterval[1] = max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    result.push_back(newInterval);
 
-        // Case 1: no overlapping case before the merge intervals
-        // Compare ending point of intervals to starting point of newInterval
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            res.push_back(intervals[i]);
-            i++;
-        }
-
-        // Case 2: overlapping case and merging of intervals
-        while (i < n && newInterval[1] >= intervals[i][0]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        res.push_back(newInterval);
-
-        // Case 3: no overlapping of intervals after newinterval being merged
-        while (i < n) {
-            res.push_back(intervals[i]);
-            i++;
-        }
-        return res;
+    // case 3
+    while(i < n) {
+        result.push_back(intervals[i]);
+        i++;
+    }
+    return result;
     }
 };
