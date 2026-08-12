fe661a193d56c6f3403ac9d2f4622829f715ce92
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-13 00:59:41 +0530

    Time: 13 ms (31.66%), Space: 7.7 MB (89.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
+    // using recursion
     if(n <= 1) return n;
-    return fib(n-1) + fib(n-2);    
+    return fib(n - 1) + fib(n - 2);    
     }
 };
