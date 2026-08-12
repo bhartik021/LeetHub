3d92bab1aaedc11869df12b15af960c71663d4db
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-13 01:35:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.4%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int f(int n, vector<int>&dp) {
         if(n <= 1) return n;
-        if(dp[n] != -1) return dp[n];  
+        if(dp[n] != -1) return dp[n];
         return dp[n] = f(n - 1, dp) + f(n - 2, dp);
     }
 
     int fib(int n) {
-    // memoization // top-down dp
-    vector<int>dp(n + 1 , -1);
-    return f(n, dp);
+    // memoization , top-down dp 
+    vector<int>dp(n + 1, -1);
+    return f(n, dp);    
     }
 };
