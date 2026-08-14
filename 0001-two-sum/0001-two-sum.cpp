7a91943e7b16c734c7a0a342d6ea22865998b11c
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-14 10:16:00 +0530

    Time: 4 ms (51.7%), Space: 16 MB (5.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    // time complexity O(n)^2
-    // for(int i = 0; i <= nums.size() - 2; i++) {
-    //     for(int j = i + 1; j <= nums.size() - 1; j++) {
-    //         if(nums[i] + nums[j] == target) return {i, j};
-    //     }
-    // }    
-    // return {-1, -1};
-
-
-    // using hashtable
-
     unordered_map<int, int>mp;
-    // firstly we will insert elements along with its index in map
     for(int i = 0; i < nums.size(); i++) {
         mp[nums[i]] = i;
-    }
-    // now we will iterate over the array elements 
-    for(int i = 0; i < nums.size(); i++) {
-        // here we will store target - current element subtraction
+    }    
+
+    for(int i = 0; i <= nums.size(); i++) {
         int check = target - nums[i];
-        // here will check if this subtraction exists in the map that should not be equal to its index
         if(mp.count(check) && mp[check] != i) {
-            return {i, mp[check]};
+            return {i, mp[check]}; 
         }
     }
     return {-1, -1};
