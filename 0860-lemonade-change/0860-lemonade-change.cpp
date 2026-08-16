d98df6f7a220ee3e07b0e6b0081f993c8145f35b
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-17 02:15:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-    // time complexity => O(n)
+    sort(bills.begin(), bills.end());
     int five = 0, ten = 0;
     for(int i = 0; i < bills.size(); i++) {
-        // case 1 : if the bill is 5 , i'll take 5
-        if(bills[i] == 5) five++;
 
-        // case 2 : if the bill is 10 then i'll take 10 and reduce 5 from 5
+        if(bills[i] == 5) {
+            five++;
+        }
+        
         else if(bills[i] == 10) {
             if(five > 0) {
-                five--;
                 ten++;
-            } else {
+                five--;
+            }else {
                 return false;
             }
-
-        // case 3 : if the bill is 20 then 
-        }else if(bills[i] == 20) {
-            // either i should  have conbination of 5 and 10 , in the case i give 1 -> 10 and 1 -> 5
+        }
+        
+        else if(bills[i] == 20){
             if(five > 0 && ten > 0) {
-                five--;
                 ten--;
-            // and if i have 3+ fives then i'll give 3 five and reduce 3 fives from fives not more than that
-            }else if (five >= 3) {
+                five--;
+            }else if(five >= 3) {
                 five -= 3;
             }else {
                 return false;
             }
         }
-    }    
-    return true;
+    }   
+
+    return true; 
     }
 };
