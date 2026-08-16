47c7450381a357193d9fd3d32db7cc44417d2b1a
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-16 20:35:39 +0530

    Time: 0 ms (100%), Space: 10 MB (64.79%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,16 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-    // time complexity => O(n) // because fast pointers traverse whole linked list
-    // space complexity => O(1) // becuase we are using two points only and no other data structure
     ListNode* slow = head;
     ListNode* fast = head;
 
     while(fast != NULL && fast->next != NULL) {
         slow = slow->next;
         fast = fast->next->next;
-    } 
+    }    
 
-    return slow;   
+    return slow;
     }
 };
