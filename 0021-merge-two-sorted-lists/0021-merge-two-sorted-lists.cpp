8fd3ec3a868f889df76fa184533c30ced2324eee
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-16 21:14:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (63.3%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -13,21 +13,41 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
     // t.c => O(n + m)
     // s.c => )(n + m)
-    ListNode* list3;
-    if(!list1 && !list2) return NULL;
-    if(!list1) return list2;
-    if(!list2) return list1;
+    // ListNode* list3;
+    // if(!list1 && !list2) return NULL;
+    // if(!list1) return list2;
+    // if(!list2) return list1;
 
-    if(list1->val < list2->val){
-        list3 = list1;
-        list3->next = mergeTwoLists(list1->next, list2);
-    }    
+    // if(list1->val < list2->val){
+    //     list3 = list1;
+    //     list3->next = mergeTwoLists(list1->next, list2);
+    // }    
 
-    else {
-        list3 = list2;
-        list3->next = mergeTwoLists(list2->next, list1);
-    }
+    // else {
+    //     list3 = list2;
+    //     list3->next = mergeTwoLists(list2->next, list1);
+    // }
     
-    return list3;
+    // return list3;
+
+    // t.c => O(n + m)
+    // s.c => O(n)
+
+    ListNode dummy(-1);
+    ListNode* current = &dummy;
+
+    while(list1 && list2) {
+        if(list1->val < list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        }else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+
+        current = current->next;
+    }
+    current->next = list1 ? list1 : list2;
+    return dummy.next;
     }
 };
