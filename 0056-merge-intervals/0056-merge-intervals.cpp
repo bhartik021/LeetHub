beb1822a7223076f0e5313d895d738d6de155b97
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-17 11:41:33 +0530

    Time: 9 ms (16.14%), Space: 23.7 MB (95.46%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    // custom comparator to sort 2D vector based on the start time of eacg interval in assceding order
     static bool compare(const vector<int>&a, const vector<int>&b) {
         return a[0] < b[0];
     }
-
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-    // grredy approach 
-    // time complexity => O(n log n)
-    // space complexity => O(n)
-    // create an vector of merged intervals that needs to be return
-    vector<vector<int>>mergedIntervals;
-
-    // will sort intervals based on the start time of each intervals along with custom comparator
-    sort(intervals.begin(), intervals.end(), compare);
-
-    // first will push first interval in mergedIntervals vector
-    mergedIntervals.push_back(intervals[0]);
-
-    // iterate over the all intervals
+    vector<vector<int>>mergeIntervals;
+    sort(intervals.begin(), intervals.end(), compare);  
+    mergeIntervals.push_back(intervals[0]);
     for(int i = 1; i < intervals.size(); i++) {
-        int lastIntervalEndingTime = mergedIntervals.back()[1];
-        //  if current interval start time is lesser than ending time of previos interval
-        if(intervals[i][0] <= lastIntervalEndingTime) {
-            mergedIntervals.back()[1] = max(lastIntervalEndingTime, intervals[i][1]);
-        }else {
-            // if there is no overlaap simply push that interval in mergedIntervals vector
-            mergedIntervals.push_back(intervals[i]); 
+        int lastEndingtime = mergeIntervals.back()[1];
+        if(intervals[i][0] <= lastEndingtime) {
+            mergeIntervals.back()[1] = max(lastEndingtime, intervals[i][1]);
+        } else {
+            mergeIntervals.push_back(intervals[i]);
         }
     }
-    return mergedIntervals;
+    return mergeIntervals;
     }
 };
