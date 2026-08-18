5a5c2aa8b121f7a47301d892af1e10a0d700be02
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-08-19 00:10:52 +0530

    Time: 7 ms (5.07%), Space: 11.4 MB (5.21%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
+    // a very basic problem using set to store all 3 unique characters in a single substring
     int count = 0;
+    // if substring is less then 3 which is a basic requirmenet to return the ans then we will reutrn 0
     if(s.size() < 3) return 0;
-    for(int i = 0; i <= s.size() - 3; i++) {
-        unordered_set<char>st;
-        st.insert(s[i]);
-        st.insert(s[i + 1]);
-        st.insert(s[i + 2]);
 
-        if(st.size() == 3) {
-            count++;
-        }
+    for(int i = 0; i <= s.size() - 3; i++) {
+        unordered_set<char>seen;
+        // will insert all 3 chracters if they are unique for particular substring of 3 length
+        seen.insert(s[i]);
+        seen.insert(s[i + 1]);
+        seen.insert(s[i + 2]);
+
+        // as soon our set size is equal to 3 will increase the count for each substring
+        if(seen.size() == 3) count++;
+
     }    
+
     return count;
     }
 };
-
-// aababcabc
-// abc
-// bca
-// cab
-// abc
