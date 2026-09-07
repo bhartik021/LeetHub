a422e68b83d3e321e682e5ad65fbc56116c2bcde
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2026-09-07 23:03:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.45%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x != 0) {
+
+        // accessing the last digit
+        int digit = x % 10;
+
+        if(ans > INT_MAX / 10 || ans == INT_MAX / 10 && digit > 7) return 0;
+
+        if(ans < INT_MIN / 10 || ans == INT_MIN / 10 && digit < -8) return 0;
+
+        // adding last digit in ans
+        ans = ans * 10 + digit;
+
+        // removing the last digit
+        x /= 10;
+        }
+        
+        return ans;
+    }
+};
